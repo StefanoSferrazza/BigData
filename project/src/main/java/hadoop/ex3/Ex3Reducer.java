46293398108ac05364f637fb06b13d0c826beadd
TeodoroBaldazzi <teo.baldazzi@stud.uniroma3.it>
2020-05-20 18:27:44 +0200

    Hive: job3 minor fix

--- project/src/main/java/hadoop/ex3/Ex3Reducer.java
+++ project/src/main/java/hadoop/ex3/Ex3Reducer.java
@@ -24,22 +24,24 @@ public class Ex3Reducer extends Reducer<Text,Text,Text,Text>{
 
 	private static final String COMMA = ",";
 
-	private Map<String,Integer> numCompanies = new HashMap<>();
-	private Map<String,Text> companiesQuotations = new HashMap<>();
+	private Map<String,Integer> companiesNumbers = new HashMap<>();
+	private Map<String,String> companiesQuotations = new HashMap<>();
 
 
 	@Override
 	protected void setup(Context context) throws IOException, InterruptedException{
 
-		context.write(new Text( " { COMPANIES_COMMON_TREND }:") , new Text("2016: ANN_VAR%" + COMMA + "2017: ANN_VAR%" + COMMA + "2018: ANN_VAR%"));
+		context.write(new Text( "{COMPANIES_COMMON_TREND}:") , new Text("2016: ANN_VAR%" + COMMA + "2017: ANN_VAR%" + COMMA + "2018: ANN_VAR%"));
 	}
+
+
 
 	@Override
 	protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 
 		String outputKey = "{";
 		boolean isFirst = true;
-		int numbCompanies = 0;
+		int numCompanies = 0;
 
 		for(Text t : values) {
 			if(isFirst) {
@@ -48,13 +50,14 @@ public class Ex3Reducer extends Reducer<Text,Text,Text,Text>{
 			}
 			else
 				outputKey += COMMA + t;
-			numbCompanies++;			
+			numCompanies++;			
 		}
 		outputKey += "}:";
 
-		if(numbCompanies > 1) {
-			this.numCompanies.put(outputKey, numbCompanies);
-			this.companiesQuotations.put(outputKey, key);
+		if(numCompanies > 1) {
+			String yearsQuots = key.toString();
+			this.companiesNumbers.put(outputKey, numCompanies);
+			this.companiesQuotations.put(outputKey, yearsQuots);
 		}
 	}
 
@@ -62,11 +65,11 @@ public class Ex3Reducer extends Reducer<Text,Text,Text,Text>{
 
 	@Override
 	protected void cleanup(Context context) throws IOException, InterruptedException {
-		this.numCompanies = Utilities.sortByValue(numCompanies);
+		this.companiesNumbers = Utilities.sortByValue(companiesNumbers);
 		
-		for(String comps : this.numCompanies.keySet()) {
+		for(String comps : this.companiesNumbers.keySet()) {
 			Text outputKey = new Text(comps);
-			Text trend = this.companiesQuotations.get(comps);
+			Text trend = new Text(this.companiesQuotations.get(comps));
 			context.write(outputKey, trend);
 		}
 	}
