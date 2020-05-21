e622f1ac2a8122002190aaa387c1c89d7a834712
Stefano <ste.sferrazza@gmail.com>
2020-05-21 18:08:29 +0200

    checkers

---
+++ project/src/main/java/utilities/CheckerHS.java
@@ -0,0 +1,56 @@
+package utilities;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SparkSession;
+
+public class CheckerHS {
+	
+	public static void main(String[] args) throws IOException {
+
+		String inputPath = args[0];
+		String outputPath = args[1];
+		
+		SparkSession session = SparkSession
+				.builder()
+				.appName("CheckerHS")
+				.getOrCreate();
+		
+		Function<String,Boolean> checkInputHS = 
+				line ->	{
+					try {
+						String[] tokens = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+						if(tokens.length==5 &&
+								Utilities.inputExists(tokens[0]) &&		//ticker
+//								Utilities.inputExists(tokens[1]) &&		//exchange
+								Utilities.inputExists(tokens[2]) &&		//azienda
+								Utilities.inputExists(tokens[3])// &&	//settore
+								/*Utilities.inputExists(tokens[4])*/) {	//industria
+							return true;
+						}
+						else return false;
+					}
+					catch(Exception e) {
+						return false;
+					}
+		};
+		
+		JavaRDD<String> results = session.read().textFile(inputPath).javaRDD().filter(checkInputHS);
+	
+		List<String> filtered = results.collect();
+		
+		FileWriter writer = new FileWriter(outputPath); 
+
+		for(String line: filtered) {
+        	writer.write(line + System.lineSeparator());
+        }
+		
+		writer.close();
+
+		session.stop();
+	}
+}
---
+++ project/src/main/java/utilities/CheckerHSP.java
@@ -0,0 +1,67 @@
+package utilities;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SparkSession;
+
+public class CheckerHSP {
+	
+	private static final String COMMA = ",";
+		
+	public static void main(String[] args) throws IOException {
+
+		String inputPath = args[0];
+		String outputPath = args[1];
+		
+		SparkSession session = SparkSession
+				.builder()
+				.appName("CheckerHSP")
+				.getOrCreate();
+		
+		Function<String,Boolean> checkInputHSP = 
+				line ->	{
+							try {
+								String[] tokens = line.split(COMMA);
+								if(tokens.length==8 &&
+										Utilities.inputExists(tokens[0]) &&		//ticker
+//										Utilities.inputExists(tokens[1]) &&		//open
+										Utilities.inputExists(tokens[2]) &&		//close
+//										Utilities.inputExists(tokens[3]) &&		//adj_close
+//										Utilities.inputExists(tokens[4]) &&		//low
+//										Utilities.inputExists(tokens[5]) &&		//high
+										Utilities.inputExists(tokens[6]) &&		//volume
+										Utilities.inputExists(tokens[7])) {		//date
+									Float close = Float.parseFloat(tokens[2]);
+									Long volume = Long.parseLong(tokens[6]);
+									LocalDate date = LocalDate.parse(tokens[7]);
+									if(date.getYear()>=2008 && date.getYear()<=2018 ) {
+										return true;
+									}
+								}
+								return false;
+							}
+							catch(Exception e) {
+								return false;
+							}
+		};
+		
+		JavaRDD<String> results = session.read().textFile(inputPath).javaRDD().filter(checkInputHSP);
+	
+		List<String> filtered = results.collect();
+		
+		FileWriter writer = new FileWriter(outputPath); 
+
+		for(String line: filtered) {
+        	writer.write(line + System.lineSeparator());
+        }
+		
+		writer.close();
+
+		session.stop();
+	}
+}
